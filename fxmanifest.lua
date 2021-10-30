16048e2f4787ef23c78c0eaac30556d9da21c893
Keepcool <64145740+rldevelopement@users.noreply.github.com>
2021-10-30 13:45:24 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'A Loading Screen Made by keepcool (https://github.com/rldevelopement/)'
+
+version '1.0.0'
+
+files {
+	"index.html",
+	"img/*.jpg",
+	"img/*.png",
+	"css/*.css",
+	"js/*.js"
+	}
+	
+loadscreen "index.html"
